7c62a5341e5883a42ef1c6f62f5c67ddf1020eb7
Brahms <brahmsisme@gmail.com>
2023-11-21 00:47:03 +0100

    PlayerDTO

---
+++ src/main/java/be/brahms/models/dtos/PlayerDTO.java
@@ -0,0 +1,20 @@
+package be.brahms.models.dtos;
+
+import be.brahms.enums.Gender;
+import be.brahms.models.entities.PlayerEnt;
+
+import java.time.LocalDate;
+
+public record PlayerDTO(
+
+        Long id,
+        String pseudo,
+        String email,
+        LocalDate birthdate,
+        int elo,
+        Gender gender
+) {
+    public static PlayerDTO fromEntity(PlayerEnt player){
+        return new PlayerDTO(player.getId(), player.getPseudo(), player.getEmail(), player.getBirthdate(), player.getElo(), player.getGender());
+    }
+}
