fcb0a249c052328f50d67941286b7112b35d8fd3
Brahms <brahmsisme@gmail.com>
2023-11-23 12:09:00 +0100

    add service to Tournament

---
+++ src/main/java/be/brahms/services/TournamentServ.java
@@ -0,0 +1,8 @@
+package be.brahms.services;
+
+import be.brahms.models.entities.TournamentEnt;
+
+public interface TournamentServ {
+
+    TournamentEnt create(TournamentEnt tournament);
+}
---
+++ src/main/java/be/brahms/services/impl/TournamentServImpl.java
@@ -0,0 +1,33 @@
+package be.brahms.services.impl;
+
+import be.brahms.enums.Status;
+import be.brahms.models.entities.TournamentEnt;
+import be.brahms.repositories.TournamentRepo;
+import be.brahms.services.TournamentServ;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class TournamentServImpl implements TournamentServ {
+
+    private final TournamentRepo tournamentRepo;
+
+    public TournamentServImpl(TournamentRepo tournamentRepo) {
+        this.tournamentRepo = tournamentRepo;
+    }
+
+    @Override
+    public TournamentEnt create(TournamentEnt tournament) {
+        LocalDate currentDate = LocalDate.now();
+        Status currentStatus = Status.WAITING;
+        int minimumPlayer = tournament.getMinPlayer();
+
+        tournament.setCreateDateAt(currentDate);
+        tournament.setEndDateAt(currentDate.plusDays(minimumPlayer));
+        tournament.setStatus(currentStatus);
+
+
+        return tournamentRepo.save(tournament);
+    }
+}
